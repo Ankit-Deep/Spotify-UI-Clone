0807e13579da53f779e7ff9a5e36e1527e5c4e58
Ankit Deep <AnkitDeep01@outlook.com>
2024-09-01 02:38:31 +0530

    mouseenter works for play button but not on mouseleave

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const playDivs = document.querySelectorAll('.play');
 
-playDivs.forEach(e => {
-    let playSign = document.createElement('i');
+playDivs.forEach(evt => {
+    const playSign = document.createElement('i');
     playSign.classList.add('fa-solid', 'fa-circle-play');
     playSign.style.position = 'relative';
     playSign.style.left = '40%';
-    playSign.style.bottom = '40%';
+    playSign.style.bottom = '50%';
     playSign.style.fontSize= '3vw';
     playSign.style.color = '#1ED760';
     playSign.style.backgroundColor = 'black';
     playSign.style.borderRadius = '50%';
     
-    e.addEventListener('click', (e)=> {
+    evt.addEventListener('mouseenter', (e)=> {
         let parent = e.target;
         console.log(parent);
         parent.appendChild(playSign);
-    })
 
-    e.addEventListener('dblclick', ()=> {
-        let parent = e.target;
-        parent.removeChild(playSign);
+
+        
     })
+    
+    evt.addEventListener('mouseleave  ', (e)=> {
+        let parent = e.target;
+        // parent.removeChild(playSign);
+        playSign.remove();
+    })
+    
+
 });
