5c7c26408a1d5e71ac6d6de3eabb439610350cad
Ankit Deep <AnkitDeep01@outlook.com>
2024-09-01 12:58:54 +0530

    Play button hover feature done

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const playDivs = document.querySelectorAll('.play');
 playDivs.forEach(evt => {
     const playSign = document.createElement('i');
     playSign.classList.add('fa-solid', 'fa-circle-play');
-    // playSign.style.border= '2px solid white';
-    playSign.style.position = 'relative';
-    playSign.style.left = '40%';
-    // playSign.style.top = '-40%';
-    playSign.style.fontSize= '3vw';
+    playSign.style.position = 'absolute';
+    playSign.style.left = '7.5vw';
+    playSign.style.bottom = '6.45vw';
+    playSign.style.fontSize= '3.1vw';
     playSign.style.color = '#1ED760';
     playSign.style.backgroundColor = 'black';
     playSign.style.borderRadius = '50%';
@@ -15,11 +14,10 @@ playDivs.forEach(evt => {
     const pauseSign = document.createElement('i');
     pauseSign.classList.add('fa-solid', 'fa-solid-pause');
     pauseSign.style.color= '#1ED760';
-    pauseSign.style.position = 'relative';
+    pauseSign.style.position = 'absolute';
     pauseSign.style.backgroundColor= 'black';
     pauseSign.style.fontSize= '3vw';
     pauseSign.style.borderRadius = '50%';
-
     
     evt.addEventListener('mouseenter', (e)=> {
         let parent = e.target;
@@ -30,15 +28,10 @@ playDivs.forEach(evt => {
     evt.addEventListener('mouseleave', (e)=> {
         let parent = e.target;
         playSign.remove();
-        // alert("works")
     })
 
     playSign.addEventListener("click",(e)=> {
-        playSign.classList.replace('fa-circle-play', 'fa-solid-pause');
-        let parent = e.target;
-        playSign.remove();
-        parent.appendChild(pauseSign);
-        console.log('play clicked');
+        
         
     });
     
