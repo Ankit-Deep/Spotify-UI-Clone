8e57121b170c9e8942a75e7dbff370d5a9209775
Ankit Deep <AnkitDeep01@outlook.com>
2024-09-07 02:33:15 +0530

    Pause has started working but still needs improvement

--- script.js
+++ script.js
@@ -1,40 +1,78 @@
 const playDivs = document.querySelectorAll('.play');
 
-playDivs.forEach(evt => {
+playDivs.forEach(playDiv => {
+    
+    // Play button that occurs on hover
     const playSign = document.createElement('i');
     playSign.classList.add('fa-solid', 'fa-circle-play');
+    playSign.style.display = 'none';
+    playSign.style.color = '#1ED760';
     playSign.style.position = 'absolute';
     playSign.style.left = '7.5vw';
     playSign.style.bottom = '6.45vw';
     playSign.style.fontSize= '3.1vw';
-    playSign.style.color = '#1ED760';
     playSign.style.backgroundColor = 'black';
     playSign.style.borderRadius = '50%';
-
+    playSign.style.cursor = 'pointer';
+    
+    // Pause button that occurs when clicked on play button
     const pauseSign = document.createElement('i');
-    pauseSign.classList.add('fa-solid', 'fa-solid-pause');
+    pauseSign.classList.add('fa-solid', 'fa-circle-pause');
+    pauseSign.style.display = 'none';
     pauseSign.style.color= '#1ED760';
     pauseSign.style.position = 'absolute';
+    pauseSign.style.left = '7.5vw';
+    pauseSign.style.bottom = '6.45vw';
+    pauseSign.style.fontSize= '3.1vw';
     pauseSign.style.backgroundColor= 'black';
-    pauseSign.style.fontSize= '3vw';
     pauseSign.style.borderRadius = '50%';
+    pauseSign.style.zIndex = 2;
     
-    evt.addEventListener('mouseenter', (e)=> {
+    
+    playDiv.addEventListener('mouseenter', (e)=> {
         let parent = e.target;
-        console.log(parent);
-        parent.appendChild(playSign);
+        playDiv.appendChild(playSign);
+        playSign.style.display = 'block';
     })
     
-    evt.addEventListener('mouseleave', (e)=> {
+    playDiv.addEventListener('mouseleave', (e)=> {
         let parent = e.target;
         playSign.remove();
+        // playDiv.appendChild(pauseSign);
+        // playSign.style.display = 'none';
+        // pauseSign.style.display = 'block';
+
     })
 
-    playSign.addEventListener("click",(e)=> {
+    playSign.addEventListener('click', (e)=> {
+        playSign.style.display = 'none';
         
-        
-    });
-    
+        pauseSign.style.display = 'block';
+        playDiv.appendChild(pauseSign);
 
+    })
+    
 });
+
+// playSign.addEventListener("click", (e)=> {
+//     console.log("play button");
+    
+    // console.log(e);
+    // console.log(e.target.addEventListener);
+    
+    
+    // e.target.addEventListener("click", ()=> {
+    //     playSign.style.display = 'none';
+    //     pauseSign.style.display = 'block';
+
+    // })
+    
+// })
+
+
+
+function playMusic() {
+    
+    
+}
 
